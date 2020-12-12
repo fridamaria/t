72d583200824da5f6d873c9df75208c2c4ab6c6c
Frida Eriksson <fridaeriksson@MBPsomthorFrida.lan>
2020-12-12 12:47:34 +0100

    stripping recipe of unwanted tags

--- src/components/TacoRecipe.jsx
+++ src/components/TacoRecipe.jsx
@@ -3,10 +3,19 @@ import styled from 'styled-components'
 import parse from 'html-react-parser'
 import { MDToHTML } from 'utils/Utils'
 
-const RecipeOuter = styled.div``
+const RecipeOuter = styled.div`
+  border-top: 1px solid #e5e5e5;
+  padding: 60px 0px;
+  margin: 0px 16px;
+`
 
 export const TacoRecipe = ({ recipe }) => {
   const [recipeHTML, setRecipeHTML] = useState('')
+  const recipeStripped = recipeHTML
+    .replace(/<h1>(.*)<\/h1>/g, '') // remove h1-tags
+    .replace(/<a[^>]*>|<\/a>/g, '') // removes unwanted a-tags
+    .replace(/<p><img[^>]*><\/p>/g, '') // removes unwanted imgs
+    .replace(/<hr>/g, '') // removes unwanted hr-tags
 
   useEffect(() => {
     MDToHTML(recipe)
@@ -17,7 +26,8 @@ export const TacoRecipe = ({ recipe }) => {
 
   return (
     <RecipeOuter>
-      {parse(recipeHTML)}
+      <h2>Recipe</h2>
+      {parse(recipeStripped)}
     </RecipeOuter>
   )
 }
